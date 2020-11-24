ec5fd7f05739fdac9941c472d3e0a3a8cdb524cd
Charly_Angel <c.a.a.a.503@gmail.com>
2020-11-23 19:39:05 -0600

    fix: corrección del nombre de la variable userController a UserController

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -4,17 +4,17 @@ const resolvers = {
   Query: {
     // User
     getUser: (_, { id, username }) => UserController.getUser(id, username),
-    search: (_, { search }) => userController.search(search),
+    search: (_, { search }) => UserController.search(search),
   },
   Mutation: {
     // User
     register: (_, { input }) => UserController.register(input),
     login: (_, { input }) => UserController.login(input),
     updateAvatar: (_, { file }, context) =>
-      userController.updateAvatar(file, context),
-    deleteAvatar: (_, {}, context) => userController.deleteAvatar(context),
+      UserController.updateAvatar(file, context),
+    deleteAvatar: (_, {}, context) => UserController.deleteAvatar(context),
     updateUser: (_, { input }, context) =>
-      userController.updateUser(input, context),
+      UserController.updateUser(input, context),
   },
 };
 
