73a84ac3f1495853c0247a8b416d565d91e95088
Charly_Angel <c.a.a.a.503@gmail.com>
2021-01-07 10:19:45 -0600

    refactor: separar la conexión a la bd

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
 const { port } = require("./config");
-const mongoose = require("mongoose");
-const MONGO_URI = require("./lib/mongo");
+const dbConnection = require("./lib/mongo");
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./gql/schema");
 const resolvers = require("./gql/resolvers");
 const { compareToken } = require("./utils/token");
-
-mongoose.connect(
-  MONGO_URI,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true
-  },
-  (err, _) => {
-    if (err) {
-      console.log("error de conexión");
-    } else {
-      server();
-    }
-  }
-);
 
 function server() {
   const serverApollo = new ApolloServer({
@@ -30,9 +12,10 @@ function server() {
     context: ({ req }) => compareToken(req.headers.authorization)
   });
 
-  serverApollo.listen({ port }).then(({ url }) => {
-    console.log("#################################");
-    console.log("servidor apollo on " + url);
-    console.log("#################################");
+  serverApollo.listen({ port }).then(async ({ url }) => {
+    await dbConnection();
+    console.log(`Listening ${url}`);
   });
 }
+
+server();
