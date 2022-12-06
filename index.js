e6b50ff92ca1d4bb12127b3ab73f686f653dcf84
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-12-05 23:38:47 -0600

    feat: activar el playground

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const { port } = require("./config");
-const dbConnection = require("./lib/mongo");
-const { ApolloServer } = require("apollo-server");
-const typeDefs = require("./gql/schema");
-const resolvers = require("./gql/resolvers");
-const { compareToken } = require("./utils/token");
+const { port } = require('./config');
+const dbConnection = require('./lib/mongo');
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./gql/schema');
+const resolvers = require('./gql/resolvers');
+const { compareToken } = require('./utils/token');
 
 function server() {
   const serverApollo = new ApolloServer({
     typeDefs,
     resolvers,
-    context: ({ req }) => compareToken(req.headers.authorization)
+    introspection: true,
+    playground: true,
+    context: ({ req }) => compareToken(req.headers.authorization),
   });
 
   serverApollo.listen({ port }).then(async ({ url }) => {
